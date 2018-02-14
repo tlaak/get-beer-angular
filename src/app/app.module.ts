b830b38be5d7346952be1011e72afa68239f7858
Timo Laak <tlaak@users.noreply.github.com>
2018-02-14 16:40:14 +0100

    Beer effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { GetBeerButtonComponent } from './components/get-beer-button/get-beer-button.component';
@@ -14,6 +15,7 @@ import { BeerService } from 'app/services/beer.service';
     BrowserModule,
     StoreModule.forRoot([]),
     HttpClientModule,
+    EffectsModule.forRoot([BeerEffects]),
     StoreDevtoolsModule.instrument({
       maxAge: 50, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
---
+++ src/app/effects/beer.effects.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Action } from '@ngrx/store';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { catchError, map, mergeMap, tap } from 'rxjs/operators';
+import { BeerService } from 'app/services/beer.service';
+
+@Injectable()
+export class BeerEffects {
+  constructor(private actions$: Actions, private beerService: BeerService) {}
+
+  // Listen for the 'LOGIN' action
+  @Effect()
+  login$: Observable<Action> = this.actions$.pipe(
+    ofType('GET_BEER'),
+    tap(foo => console.log('get beer called')),
+    mergeMap(action =>
+      this.beerService
+        .getBeer()
+        .pipe(
+          map(beer => ({ type: 'GOT_BEER', payload: beer })),
+          catchError(() => of({ type: 'FAILED_TO_GET_BEER' }))
+        )
+    )
+  );
+}
