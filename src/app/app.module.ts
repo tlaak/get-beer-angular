458a1b656d3802199e5ede9fc67ab8a469880a96
Timo Laak <tlaak@users.noreply.github.com>
2018-02-14 14:06:27 +0100

    Include store in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { GetBeerButtonComponent } from './components/get-beer-button/get-beer-button.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, GetBeerButtonComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 50, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
