d8d4fec8011284ea28cd6b2889e7585b9f121411
Timo Laak <tlaak@users.noreply.github.com>
2018-02-14 16:40:54 +0100

    Beer component

--- src/app/components/get-beer-button/get-beer-button.component.ts
+++ src/app/components/get-beer-button/get-beer-button.component.ts
@@ -1,18 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs/Subscription';
+import { GET_BEER } from 'app/reducers/beer.reducer';
 
 @Component({
   selector: 'app-get-beer-button',
   templateUrl: './get-beer-button.component.html',
   styleUrls: ['./get-beer-button.component.css'],
 })
-export class GetBeerButtonComponent implements OnInit {
-  constructor() {}
+export class GetBeerButtonComponent implements OnInit, OnDestroy {
+  private beerSub: Subscription;
 
-  ngOnInit() {}
+  public beers: Beer[];
+  public isLoading: boolean;
+
+  constructor(private store: Store<any>) {}
+
+  ngOnInit() {
+    this.beerSub = this.store
+      .select('beers')
+      .subscribe((beerState: BeerState) => {
+        console.log(beerState);
+        this.beers = beerState.beers;
+        this.isLoading = beerState.isLoading;
+      });
+  }
+
+  ngOnDestroy() {
+    this.beerSub.unsubscribe();
+  }
 
   getBeer(event: Event) {
     event.preventDefault();
-
-    console.log('Get Beer clicked');
+    this.store.dispatch({ type: GET_BEER });
   }
 }
