fdeafe1b6d4a786e0213d9e3450ab3b49cd53585
Timo Laak <timo.laak@daimler.com>
2018-02-15 11:56:32 +0100

    Fixed the get-beer component tests

--- src/app/components/get-beer/get-beer.component.spec.ts
+++ src/app/components/get-beer/get-beer.component.spec.ts
@@ -1,14 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { GetBeerComponent } from './get-beer.component';
+import { StoreModule, combineReducers } from '@ngrx/store';
+import { beerReducer } from 'app/reducers/beer.reducer';
 
 describe('GetBeerComponent', () => {
   let component: GetBeerComponent;
   let fixture: ComponentFixture<GetBeerComponent>;
 
   beforeEach(
+    /**
+     * This component is subscribing to the @ngrx store. You need to use the
+     * StoreModule and define the name for the store in TestBed configuration
+     */
     async(() => {
       TestBed.configureTestingModule({
+        imports: [
+          StoreModule.forRoot({
+            beers: combineReducers(beerReducer),
+          }),
+        ],
         declarations: [GetBeerComponent],
       }).compileComponents();
     })
@@ -20,7 +30,20 @@ describe('GetBeerComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a subscription to beer reducer', () => {
+    const beerSub = component['beerSub'];
+    expect(beerSub).toBeDefined();
+    expect(beerSub.closed).toBeFalsy();
+  });
+
+  it('should have no subscriptions when component has been destroyed', () => {
+    component.ngOnDestroy();
+
+    const beerSub = component['beerSub'];
+    expect(beerSub.closed).toBeTruthy();
   });
 });
