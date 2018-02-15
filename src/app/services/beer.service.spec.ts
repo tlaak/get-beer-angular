4e1530ef12d431a2120c9db8c5eab5bce79ec1cc
Timo Laak <timo.laak@daimler.com>
2018-02-15 11:56:40 +0100

    Fixed the beer service test

--- src/app/services/beer.service.spec.ts
+++ src/app/services/beer.service.spec.ts
@@ -1,10 +1,17 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { BeerService } from './beer.service';
 
 describe('BeerService', () => {
   beforeEach(() => {
+    /**
+     * You have to import HttpClientModule and define it in the TestBed
+     * configuration. It's a dependency of the service so without this step
+     * you will get "No provider for HttpClient!" error.
+     */
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       providers: [BeerService],
     });
   });
